0537c09840546e9761d5ea78821cbad6b52300b3
Tony Brown <tonyb@mr-itpro.com>
2021-03-09 21:01:59 -0700

    Add files via upload

---
+++ TroubleShoot.ps1
@@ -0,0 +1,100 @@
+<#
+.SYNOPSIS
+get server info to trouble shoot it Updates & Logs !!! FUN STUFF
+.EXAMPLE
+'one', 'two', 'three' TroubleShoot.ps1
+.EXAMPLE
+TroubleShoot.ps1 -computername localhost
+.EXAMPLE
+TroubleShoot.ps1 -computeername one, two, three
+.EXAMPLE
+get-content <ServerList.txt> or <ServerList.csv> | TroubleShoot.ps1
+.PARAMETER computername
+one or more computername, or IP address... peace to America!
+.LINK
+        http://www.mr-itpro.com
+ #>
+
+
+[CmdletBinding(HelpURI='http://google.com')]
+ Param(
+                [Parameter(Mandatory=$true,ValuefromPipeline=$true, HelpMessage="Please enter the Server name or IP address you want to view info about")]
+                [string []]$computername,
+                [string]$errorlogpath ='c:\temp\Trouble_Shoot_.log'
+ 
+ 
+)
+
+
+
+begin{
+$host.UI.RawUI.WindowTitle = "Trouble Shooting Info "
+
+$day = Read-Host "how many days do you want to go back for patches"
+
+}
+
+
+
+
+process{
+    foreach ($computer in $computername) {
+
+
+        Write-Verbose "about to query $computer"
+        try    {
+                $who = Get-WMIObject Win32_ComputerSystem -computername $computer| Select-Object -ExpandProperty name 
+        
+               } catch {
+                            $computer | Out-File $errorlogpath -append
+                        }
+                    $starttime =Get-Date
+                   
+                    $configs= Get-WindowsFeature | ? { $_.Installed } | ft -AutoSize  
+                    $mem=[math]::Round((Get-WmiObject -Class Win32_ComputerSystem  -computer $computer ).TotalPhysicalMemory/1GB) 
+
+                     Write-Host ""
+                    
+                    Get-HotFix -ComputerName $computer |?{$_.InstalledOn -gt ((Get-Date).AddDays(-$day))} | ft -AutoSize
+
+                    $patchUpdates = (get-hotfix -ComputerName $computer -Description update*  | sort installedon)[-1] | ft -AutoSize
+
+
+                    $processesors = Get-WmiObject –class Win32_processor -ComputerName $computer   | select Name, NumberOfCores,NumberOfEnabledCore,NumberOfLogicalProcessors | ft -AutoSize 
+
+                    $logviewError = Get-EventLog -ComputerName $computer -LogName System  -after ([datetime]::Today)   | Where-Object {$_.EntryType -like 'Error' } | ft -AutoSize 
+
+                    $logviewCritical = Get-EventLog -ComputerName $computer -LogName System  -after ([datetime]::Today) | Where-Object{$_.EntryTypentryType -like 'Critical'} | ft -AutoSize 
+
+                    $logviewWarning = Get-EventLog -ComputerName $computer -LogName System  -after ([datetime]::Today)   | Where-Object {$_.EntryType -like 'Warning' } | ft -AutoSize 
+
+
+                    Write-Host ""
+                    Write-Host "#######################" -ForegroundColor DarkGreen
+                    write-output $computer 
+                    Write-Host "#######################" -ForegroundColor DarkGreen
+
+                    Write-Output $processesors
+
+                    Write-Output "$mem GB of Ram on $computer"
+
+                    write-output $configs
+
+                    write-output $computer  $patchSecurity
+                    write-output $patchUpdates 
+
+                    write-output $logviewCritical
+                    write-output $logviewWarning
+                    write-output $logviewError
+
+
+           
+        }
+}
+End{
+
+                    $endtime =Get-Date
+                    $total= $endtime -$starttime
+                    Write-host "Total Script time to run $total" -ForegroundColor Yellow |ft -AutoSize 
+
+}
---
+++ Uptime.ps1
@@ -0,0 +1,69 @@
+﻿ <#
+ .SYNOPSIS
+ Gets COMPUTER UP Time server  STATUS !!! FUN STUFF
+ .EXAMPLE
+ 'one', 'two', 'three' UPTime.ps1
+ .EXAMPLE
+ UPTime.ps1 -computername localhost
+ .EXAMPLE
+ UPTime.ps1 -computername one, two, three
+ .EXAMPLE
+ get-content <ServerList.txt> or <ServerList.csv> | Netwmi.ps1
+ .PARAMETER computername
+ one or more computername, or IP address... peace to America!
+ #>
+ Param(
+[Parameter (Mandatory=$true,ValuefromPipeline=$true)]
+[string[]] $computername,
+[string] $errorlogpath = 'c:\temp\server_Uptime_errors.log'
+
+
+)begin{
+$host.UI.RawUI.WindowTitle = "UP Time"
+
+}
+ 
+process{
+foreach ($computer in $computername) {
+    
+                                 Write-Verbose "about to query $computer"
+         try    {
+                    $serverislive =$true
+                    $who = Get-WMIObject Win32_ComputerSystem -computername $computer | Select-Object -ExpandProperty name ​
+                                
+                    
+                 
+                                     $EndTime = get-date 
+                                     $StartTime = gwmi win32_operatingsystem -computername $computer | %{ $_.ConvertToDateTime($_.LastBootUpTime) }
+                                     $Server_uptime = New-TimeSpan -Start $StartTime -End $EndTime 
+                 } catch {
+                    $serverislive = $false
+                    Write-Verbose "$computer failed wrting to $errorlogpath  "
+                    Write-Verbose " Error was $MyErr"
+                    $computer | Out-File $errorlogpath -append         
+
+  
+                } 
+                                 
+
+                                     $props = @{ 'Number of Hours '= $Server_uptime.Minutes ;
+                                                 'Number of Minutes '=$Server_uptime.Hours ; 
+                                                 'Number of Days ' = $Server_uptime.Days ; }
+                                                  
+                                                 #'server computername' = $who ;
+                                                 #'server name you entered ' = $computer ;}
+
+
+                            
+                                     $obj = New-Object -TypeName PSObject -Property $props
+
+                                       $allthing = $Server_uptime | select Days, Hours, Minutes | Format-Table -AutoSize
+
+                                 $allthing1 =  $who  , $allthing 
+                                 Write-Host " "
+                                 Write-Output  $allthing1 
+
+
+     }
+ }
+ end{}
