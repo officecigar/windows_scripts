51fefec2dc7046ac7003d177e66265b2431ea412
Tony Brown <tonyb@mr-itpro.com>
2021-04-15 13:21:50 -0700

    Add files via upload

---
+++ network/Get_DNS_IP_Server_Search_Order.ps1
@@ -0,0 +1,3 @@
+﻿#comment 5 - Get DNS IP from Server & search order
+$srvDNS = Read-Host "Enter server to get DNS IP & search order"
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $srvDNS -Filter “IPEnabled=TRUE” | Select DnsServerSearchOrder
--- network/Test_Port_connection.ps1
+++ network/Test_Port_connection.ps1
@@ -1,6 +1,6 @@
 ﻿#comment 10 - #test port # to remote server
-$portnumber = Read-Host "enter port number"
-$server = Read-Host "enter Server number"
+$portnumber = Read-Host "enter port Number"
+$server = Read-Host "enter Server Name"
 test-NetConnection -ComputerName $server -Port $portnumber -InformationLevel "Detailed"
 
 
